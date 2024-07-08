40c958c76c070ae69181b7b3a3bd49cf8642fbaa
tech@housebanao.com <tech@housebanao.com>
2024-07-08 12:10:57 +0530

    Allowing CROCS

--- server/server.js
+++ server/server.js
@@ -5,24 +5,43 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 
 const app = express();
-const PORT = process.env.PORT || 5000; // Use 5000 as a fallback if PORT is not set
+const PORT = process.env.PORT || 5000;
 
 console.log(PORT);
+
 // Connect to MongoDB
 connectDB();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
-// Enable CORS globally for all routes
-app.use(cors({
-    origin: 'http://ec2-16-171-197-219.eu-north-1.compute.amazonaws.com:3001',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow all HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
-  }));
+
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://ec2-16-171-197-219.eu-north-1.compute.amazonaws.com:3001'
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
+
+// Debugging CORS
+app.use((req, res, next) => {
+  console.log('Incoming request origin:', req.headers.origin);
+  next();
+});
 
 // Routes
-app.use('/api/boq', require('./routes/boqRoutes')); // Ensure this route is correct
+app.use('/api/boq', require('./routes/boqRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/customers', require('./routes/customerRoutes'));
 app.use('/api/partners', require('./routes/partnerRoutes'));
@@ -34,9 +53,9 @@ app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
 // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
